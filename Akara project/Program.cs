6a38615c4f6d21046496cca77df663c9d78a8715
Abdullahi Jamilu Ohiare <113562405+Abdullahijamilu@users.noreply.github.com>
2024-08-28 11:56:40 -0700

    Add project files.

---
+++ Akara project/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+/* 
+ conole app for kiosk shop
+* order button
+* oder for both akara and pap
+* limit of order
+* current payment system
+ */
+
+
+//String order ;
+//Console.WriteLine("Place your order");
+//order = Console.ReadLine();
+
+Console.WriteLine("Selct order");
+Console.WriteLine("Input a for akara and p for pap");
+String selectOrder = Console.ReadLine().ToUpper();
+
+Console.WriteLine("Input amout of akara or pap you want to buy");
+
+Console.WriteLine("Input amout of akara or pap you want to buy");
+
+
+int limit = 5;
+
+
+while (limit>5)
+{
+    limit = 5;
+    Console.WriteLine(limit);
+    String order;
+    Console.WriteLine("Place your order");
+}
+if (limit == 5)
+{
+    Console.WriteLine("You exceeded the limit");
+    return;
+}
+{
+    string? a = null;
+    string? b = null;
+    string? order = null;
+    if (order == a)
+    {
+        Console.WriteLine("Input amout of akara or pap you want to buy");
+        return;
+    }
+    else if (order == b )
+    {
+        Console.WriteLine("Input amout of akara or pap you want to buy");
+        return;
+    }
+    else
+    {
+        Console.WriteLine("invalid order");
+    }
+}
+ if (String.Equals(selectOrder, "p"))
+{
+    Console.WriteLine("return to the order button");
+        return;
+}
+else
+{
+
+    Console.WriteLine("wrong entry");
+        return;
+}
+
---
+++ switch/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.WriteLine("Selct order");
+Console.WriteLine("Input a for akara and p for pap");
+String selectOrder = Console.ReadLine().ToUpper();
+
+Console.WriteLine("Input amout of akara or pap you want to buy");
+
+Console.WriteLine("Input amout of akara or pap you want to buy");
+
+
+int limit = 5;
+
+switch (limit)
+{
+    case 1:
+        while (limit > 5)
+        {
+            limit = 5;
+            Console.WriteLine(limit);
+            String order;
+            Console.WriteLine("Place your order");
+        }
+        if (limit == 5)
+        {
+            Console.WriteLine("You exceeded the limit");
+            return;
+        }
+        {
+            string? a = null;
+            string? b = null;
+            string? order = null;
+            if (order == a)
+            {
+                Console.WriteLine("Input amout of akara or pap you want to buy");
+                return;
+            }
+            else if (order == b)
+            {
+                Console.WriteLine("Input amout of akara or pap you want to buy");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("invalid order");
+            }
+        }
+        if (String.Equals(selectOrder, "p"))
+        {
+            Console.WriteLine("return to the order button");
+            return;
+        }
+        else
+        {
+
+            Console.WriteLine("wrong entry");
+            return;
+        }
+        break;
+}
